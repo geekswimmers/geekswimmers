8f6344ff4c1a1d4ca7cc8c65a2cfe58a11b03006
Hildeberto Mendonca <me@hildeberto.com>
2024-01-30 13:45:18 -0500

    #16 Implemented config entry to get the feedback form URL and publish it in the results page

--- web/model.go
+++ web/model.go
@@ -1,6 +1,7 @@
 package web
 
 type BaseTemplateContext struct {
+	FeedbackForm              string
 	MonitoringGoogleAnalytics string
 	Message                   string
 	Page                      any
