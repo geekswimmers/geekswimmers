aed1808c898aee44ea6bbfeb241f39c1c94dfb56
Hildeberto Mendonca <me@hildeberto.com>
2024-02-07 07:57:24 -0500

    Added test for AgeAt date

---
+++ swimmers/model_test.go
@@ -0,0 +1,35 @@
+package swimmers
+
+import (
+	"testing"
+	"time"
+)
+
+func TestAgeAt(t *testing.T) {
+
+	// Test normal case
+	swimmer := Swimmer{
+		BirthDate: time.Date(1990, 1, 1, 0, 0, 0, 0, time.UTC),
+	}
+	date := time.Date(2010, 6, 15, 0, 0, 0, 0, time.UTC)
+	expected := int64(20)
+
+	age := swimmer.AgeAt(date)
+
+	if age != expected {
+		t.Errorf("Expected %d, got %d", expected, age)
+	}
+
+	// Test edge case - birthday later in year
+	swimmer = Swimmer{
+		BirthDate: time.Date(1990, 6, 15, 0, 0, 0, 0, time.UTC),
+	}
+	date = time.Date(2010, 1, 1, 0, 0, 0, 0, time.UTC)
+	expected = 19
+
+	age = swimmer.AgeAt(date)
+
+	if age != expected {
+		t.Errorf("Expected %d, got %d", expected, age)
+	}
+}
