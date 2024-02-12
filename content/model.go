5682b672673b95c9309f1c4e10790541fcc2928a
Hildeberto Mendonca <me@hildeberto.com>
2024-02-12 08:39:33 -0500

    #24 Added abstract and highlighted columns

--- content/model.go
+++ content/model.go
@@ -3,8 +3,10 @@ package content
 import "time"
 
 type Article struct {
-	Reference string
-	Title     string
-	Published time.Time
-	Content   string
+	Reference   string
+	Title       string
+	Abstract    string
+	Highlighted bool
+	Published   time.Time
+	Content     string
 }
