5ff17ddaba0268d649158ff2cdeaca07739a9d05
Hildeberto Mendonca <me@hildeberto.com>
2024-02-12 08:23:23 -0500

    #24 Added dependence to render markdown content

--- storage/migrations/000003_blog.up.sql
+++ storage/migrations/000003_blog.up.sql
@@ -1,8 +1,10 @@
 create table if not exists article (
-	reference varchar(100) primary key,
-	title     varchar(100) not null,
-	published date         not null,
-	content   text         not null
+	reference    varchar(100) primary key,
+	title        varchar(100) not null,
+	abstract     text         not null,
+	highlighted  boolean      not null default false,
+	published    date         not null default current_date,
+	content      text         not null
 );
 
-create index idx_article_published on article (published);
+create index idx_article_published on article (highlighted, published);
