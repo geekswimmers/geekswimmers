d31ca439c6fb9c18257fd155cf370c57c452ccfc
Hildeberto Mendonca <200272+htmfilho@users.noreply.github.com>
2024-02-15 06:38:31 -0500

    Update main.js
    Removed commented code.

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,6 +1,2 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-//if (location.protocol != "https:" && location.hostname != "localhost") {
-//    window.location.replace("https://www.geekswimmers.com");
-//}
