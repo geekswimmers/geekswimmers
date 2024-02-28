175eaba39bc8a9b5494476058117b9f3785915c8
Hildeberto Mendonca <me@hildeberto.com>
2024-02-28 18:40:10 -0500

    Updated db model to add new field to article

---
+++ storage/migrations/000006_records.up.sql
@@ -0,0 +1,27 @@
+create table if not exists record_scope (
+	id       serial      primary key,
+	country  varchar(50) not null, -- CANADA
+	province varchar(50)     null, -- ONTARIO
+	region   varchar(50)     null, -- WESTERN
+    city     varchar(50)     null, -- WATERLOO
+    club     varchar(50)     null, -- ROW
+    meet     varchar(50)     null, -- DEAN_BOLE, CUNNINGHAM_CLASSIC, NOVICE_1, NOVICE_2
+);
+
+create table if not exists record (
+    id       serial       primary key,
+    scope    integer      not null references record_scope,
+    age      integer          null,
+    gender   varchar(10)      null, -- MALE, FEMALE
+    course   varchar(10)      null, -- LONG, SHORT
+    stroke   varchar(10)      null, -- FREE, BREAST, BACK, FLY, MEDLEY
+    distance integer          null
+);
+
+create table if not exists record_time (
+    id          serial      primary key,
+    record      integer     not null references record,
+    holder      varchar(50)     null,
+    record_time integer     not null,
+    record_date date            null
+);
