a0444b517d2b240e3a45cfc36a3bf2f562e54162
Hildeberto Mendonca <me@hildeberto.com>
2024-03-01 17:37:03 -0500

    Renamed table record_scope to jurisdiction and linked to time_standard

--- storage/migrations/000006_records.down.sql
+++ storage/migrations/000006_records.down.sql
@@ -1,3 +1,5 @@
+alter table time_standard drop column if exists jurisdiction;
+
 drop table if exists record_time;
 drop table if exists record;
-drop table if exists record_scope;
+drop table if exists jurisdiction;
