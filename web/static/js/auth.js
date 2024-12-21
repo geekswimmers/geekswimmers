95d02f240ffbcd421bf411672c92005fe0e840c6
Hildeberto Mendonca <me@hildeberto.com>
2024-12-21 17:39:03 -0500

    #67 Setting new password after sign up

---
+++ web/static/js/auth.js
@@ -0,0 +1,72 @@
+function checkStrength(password) {
+    let mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+    let strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+
+    let passwordStrength = document.getElementById("password-strength");
+    let passwordStrengthLabel = document.getElementById("password-strength-label");
+
+    if(strongRegex.test(password)) {
+        passwordStrength.src = "/static/images/password-strong.svg";
+        passwordStrengthLabel.textContent = "Strong";
+    } else if(mediumRegex.test(password)) {
+        passwordStrength.src = "/static/images/password-regular.svg";
+        passwordStrengthLabel.textContent = "Regular";
+    } else if (password.length === 0) {
+        passwordStrength.src = "/static/images/password-empty.svg";
+        passwordStrengthLabel.textContent = "Empty";
+    } else {
+        passwordStrength.src = "/static/images/password-weak.svg";
+        passwordStrengthLabel.textContent = "Weak";
+    }
+}
+
+document.getElementById("password").addEventListener("keyup", function () {
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirm-password").value;
+    let submitBtn = document.getElementById("submitBtn");
+
+    if (password === confirmPassword) {
+        submitBtn.disabled = false;
+    } else {
+        submitBtn.disabled = true;
+    }
+
+    checkStrength(password);
+});
+
+document.getElementById("confirm-password").addEventListener("keyup", function () {
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirm-password");
+    let submitBtn = document.getElementById("submitBtn");
+
+    if (password === confirmPassword.value) {
+        confirmPassword.classList.add("is-valid");
+        submitBtn.disabled = false;
+    } else {
+        confirmPassword.classList.remove("is-valid");
+        submitBtn.disabled = true;
+    }
+});
+
+document.getElementById("btn-signoff").addEventListener("click", function () {
+    if (confirm("Are you sure you want to sign-off?")) {
+        return true;
+    }
+    return false;
+});
+
+document.getElementById("promotional-notification").addEventListener("click", function () {
+    let xhr = new XMLHttpRequest();
+    xhr.open("PUT", "", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let successMsg = document.getElementById("notifications-saved");
+            successMsg.innerHTML = "&nbsp;Choices saved!";
+            setTimeout(function () {
+                successMsg.innerHTML = "&nbsp;";
+            }, 5000);
+        }
+    };
+    xhr.send("notification_promo=" + document.getElementById("promotional-notification").checked);
+});
